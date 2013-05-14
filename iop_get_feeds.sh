5dfa1e0eae6bad7d0e3c85b83116eaf490ae5262
Benjamin Larsson <benjamin@southpole.se>
2013-05-14 13:22:03 +0200

    Feeds script

---
+++ iop_get_feeds.sh
@@ -0,0 +1,17 @@
+./scripts/feeds update
+./scripts/feeds install asterisk18
+./scripts/feeds install libexpat
+./scripts/feeds uninstall asterisk18
+./scripts/feeds install libflac
+./scripts/feeds install libvorbis
+./scripts/feeds install libogg
+./scripts/feeds install libid3tag
+./scripts/feeds install libexif
+./scripts/feeds install libsqlite3
+./scripts/feeds install libffmpeg
+./scripts/feeds install libjpeg
+./scripts/feeds install libusb
+./scripts/feeds install samba3
+./scripts/feeds install usb-modeswitch-data
+./scripts/feeds install usb-modeswitch
+
