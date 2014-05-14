932c16f6e9af04d0e8e6952f1e727475eecb9c15
Johan Almbladh <johan.almbladh@anyfinetworks.com>
2014-05-14 17:12:47 +0200

    anyfi: Updated the Anyfi.net integration.        * Rebased the integration to our standard OpenWRT integration.        * Rebased the anyfimac multi-call binary to version 1.3.9.        * Fixed a bug in the WPA/WPA2 mode selection due to UCI naming          changes from stock OpenWRT (psk-mixed vs pskmixedpsk2).        * Updated the Anyfi.net UCI model to support the Anyfi.net          TR-069 extensions.        * Changed the compile-time Anyfi.net Controller (CONFIG_ANYFI_CONTROLLER)          default value to unset. This disables all Anyfi.net functionality          by default in standard builds.        * The new UCI model uses the option "anyfi_disabled" to disable          Anyfi.net on a radio or an interface, analogously to the standard          "disabled" option. This enables Anyfi.net by default when a          Controller is configured.

---
+++ package/anyfi/files/lib/network/anyfi_broadcom.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+#
+# Copyright (C) 2013-2014 Anyfi Networks AB.
+# Anyfi.net setup functions for Broadcom wl driver.
+
+# Get monitor name interface based for a device.
+# anyfi_dev_monitor_name <device>
+anyfi_broadcom_name_monitor() {
+	local device="$1"
+
+	# Map wlX => radiotapX
+	echo "$device" | sed 's/^.*\([0-9]\)$/radiotap\1/'
+}
+
+# Get BSS index of the last wl interface that is used (=up)
+# wl0.2 => 2
+# wl0   => (empty)
+anyfi_broadcom_get_wlindex() {
+	ifconfig | grep -o "^$1\..." | cut -d'.' -f2 | sort -n | tail -n 1
+}
+
+# Allocate virtual Wi-Fi interfaces for anyfid.
+# anyfi_broadcom_alloc_iflist <device> <bssids>
+anyfi_broadcom_alloc_iflist() {
+	local device="$1"
+	local bssids="$2"
+	local count=0
+	local wlindex num
+
+	# Enable MBSS mode if not already enabled
+	if [ "$(wlctl -i $device mbss)" = 0 ]; then
+		wlctl -i $device down
+		wlctl -i $device mbss 1
+		wlctl -i $device up
+	fi
+
+	wlindex=$(anyfi_broadcom_get_wlindex $device)
+
+	# Create WLAN interfaces and let the driver assign the BSSIDs
+	for num in $(seq $bssids); do
+		local idx=$(($wlindex + $num))
+		local wlif=$device.$idx
+
+		# Do the 'wlctl' dance to make the driver assign proper BSSIDs
+		wlctl -i $device ssid -C $idx "dummy" > /dev/null || break
+		wlctl -i $device bss -C $idx up > /dev/null
+		wlctl -i $device bss -C $idx down > /dev/null
+		wlctl -i $device ssid -C $idx "" > /dev/null
+
+		local bssid=$(wlctl -i $wlif cur_etheraddr | cut -d' ' -f2)
+		ifconfig $wlif hw ether $bssid > /dev/null
+		count=$(($count + 1))
+	done
+
+	[ "$count" -gt 0 ] && echo $device.$(($wlindex + 1))/$count
+}
+
+# Release virtual Wi-Fi interfaces allocated for anyfid.
+# anyfi_broadcom_release_iflist <device>
+anyfi_broadcom_release_iflist() {
+	true
+}
+
+# Allocate a monitor interface for anyfid.
+# anyfi_broadcom_alloc_monitor <device>
+anyfi_broadcom_alloc_monitor() {
+	local device="$1"
+	local monitor=$(anyfi_broadcom_name_monitor $device)
+
+	wlctl -i $device monitor 0 || return 1
+	wlctl -i $device monitor 3 || return 1
+	ifconfig $monitor down || return 1
+	ifconfig $monitor up || return 1
+	echo $monitor
+}
+
+# Release the monitor interface for anyfid.
+# anyfi_broadcom_release_monitor <device>
+anyfi_broadcom_release_monitor() {
+	local device="$1"
+	local monitor=$(anyfi_broadcom_name_monitor $device)
+
+	ifconfig $monitor down 2> /dev/null
+	wlctl -i $device monitor 0 2> /dev/null
+}
