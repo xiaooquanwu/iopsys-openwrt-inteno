48459d87242796d63e356a75cd5654f1eb3c0836
Benjamin Larsson <benjamin@southpole.se>
2014-09-08 15:25:19 +0200

    script: get git log from master branch

--- iop_update_package.sh
+++ iop_update_package.sh
@@ -1,5 +1,6 @@
 cd $1
-NEW_HASH=`git log -n 1 --pretty=format:"%H"`
+NEW_HASH=`git log -n 1 master --pretty=format:"%H"`
+echo $NEW_HASH
 cd ../../..
 sed -r -i  "s/^PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=$NEW_HASH/g" package/$2/Makefile
 git commit package/$2/Makefile -m "$2: update package"
