93d61a16ab91ec4114def56dcd9c910c58aef9ef
kenneth johansson <kenneth@southpole.se>
2014-09-22 14:10:17 +0200

    make sure that /etc/profile is run.
    When we start ash on serial tty make it a login shell so that /etc/profile
    is run.

--- iop_install_key.sh
+++ iop_install_key.sh
@@ -6,6 +6,6 @@ cat ~/.ssh/id_rsa.pub >>files/etc/dropbear/authorized_keys
 echo "::sysinit:/etc/init.d/rcS S boot" >files/etc/inittab
 echo "::shutdown:/etc/init.d/rcS K shutdown" >>files/etc/inittab
 echo "tty/0::askfirst:/bin/ash --login" >>files/etc/inittab
-echo "ttyS0::askfirst:/bin/ash" >>files/etc/inittab
+echo "ttyS0::askfirst:/bin/ash --login" >>files/etc/inittab
 
 echo Done
