60674db1bb5f4eefa52054df9be3d47ae37629a5
Mats Karrman <mats@southpole.se>
2015-01-16 15:55:53 +0100

    CG301 bring-up: led's and buttons, refs #6031

--- package/ledmngr/src/touch_sx9512.c
+++ package/ledmngr/src/touch_sx9512.c
@@ -89,6 +89,62 @@ static const struct i2c_reg_tab i2c_init_tab_cg300[]={
     {0x00, 0x04, 0x00 },      /* Trigger compensation */
 };
 
+/* CG301 config:
+
+   BL0: Proximity, Status white LED
+   BL1: Service button, Service white LED
+   BL2: Service red LED
+   BL3: Pair button, Pair white LED
+   BL4: Cancel button, Cancel red LED
+   BL5: Communication button, Communication white LED
+   BL6: Communication red LED
+   BL7: Unused.
+*/
+
+static const struct i2c_reg_tab i2c_init_tab_cg301[]={
+    {0xFF, 0xDE, 0x00 },      /* Reset chip */
+    {0xFF, 0x00, 0x00 },      /* Reset chip */
+
+    {0x04, 0x00, 0x00 },      /* NVM Control */
+    {0x07, 0x00, 0x00 },      /* SPO2, set as interrupt  */
+    {0x08, 0x00, 0x00 },      /* Power key ctrl */
+    {0x09, 0x78, 0x00 },      /* Irq MASK, touch+release+near+far */
+    {0x0C, 0x00, 0x00 },      /* LED map 1, none */
+    {0x0D, 0x7f, 0x00 },      /* LED map 2, BL0 -> BL6 */
+    {0x0E, 0x10, 0x00 },      /* LED Pwm Frq */
+    {0x0F, 0x00, 0x00 },      /* LED Mode */
+    {0x10, 0xFF, 0x00 },      /* Led Idle LED on */
+    {0x11, 0x00, 0x00 },      /* Led 1 off delay */
+    {0x12, 0xFF, 0x00 },      /* Led 1 on */
+    {0x13, 0x00, 0x00 },      /* Led 1 fade */
+    {0x14, 0xFF, 0x00 },      /* Led 2 on   */
+    {0x15, 0x00, 0x00 },      /* Led 2 fade */
+    {0x16, 0xFF, 0x00 },      /* Led Pwr Idle */
+    {0x17, 0xFF, 0x00 },      /* Led Pwr On */
+    {0x18, 0x00, 0x00 },      /* Led Pwr Off */
+    {0x19, 0x00, 0x00 },      /* Led Pwr fade */
+    {0x1A, 0x00, 0x00 },      /* Led Pwr On Pw */
+    {0x1B, 0x00, 0x00 },      /* Disable BL7 as power button */
+    {0x1E, 0x3b, 0x00 },      /* Cap sens enabled, bl0,bl1,bl3-bl5 */
+    {0x1F, 0x43, 0x00 },      /* Cap sens range BL0 */
+    {0x20, 0x41, 0x07 },      /* Cap sens range BL1->BL7 [20-27] */
+    {0x28, 0x02, 0x00 },      /* Cap sens thresh BL0  */
+    {0x29, 0x04, 0x07 },      /* Cap sens thresh BL1->BL7 [29-30] */
+    {0x31, 0x54, 0x00 },      /* Cap sens Op */
+    {0x32, 0x70, 0x00 },      /* Cap Sens Mode, filter 1-1/8, report all */
+    {0x33, 0x01, 0x00 },      /* Cap Sens Debounce */
+    {0x34, 0x80, 0x00 },      /* Cap Sens Neg Comp Thresh */
+    {0x35, 0x80, 0x00 },      /* Cap Sens Pos Comp Thresh */
+    {0x36, 0x17, 0x00 },      /* Cap Sens Pos Filt, hyst 2, debounce 4, 1-1/128 */
+    {0x37, 0x15, 0x00 },      /* Cap Sens Neg Filt, hyst 2, debounce 4, 1-1/32 */
+    {0x38, 0x00, 0x00 },      /* Cap Sens */
+    {0x39, 0x00, 0x00 },      /* Cap Sens Frame Skip  */
+    {0x3A, 0x00, 0x00 },      /* Cap Sens Misc  */
+    {0x3B, 0x00, 0x00 },      /* Prox Comb Chan Mask */
+    {0x3E, 0xFF, 0x00 },      /* SPO Chan Map */
+    {0x00, 0x04, 0x00 },      /* Trigger compensation */
+};
+
 /* EG300 config:
 
    BL0: Proximity, WAN green LED
@@ -153,6 +209,13 @@ static struct i2c_touch i2c_touch_list[] = {
      .irq_button = 1,
      .init_tab = i2c_init_tab_cg300,
      .init_tab_len = sizeof(i2c_init_tab_cg300)/sizeof(struct i2c_reg_tab),
+    },
+
+    {.addr = 0x2b,
+     .name = "CG301",
+     .irq_button = 1,
+     .init_tab = i2c_init_tab_cg301,
+     .init_tab_len = sizeof(i2c_init_tab_cg301)/sizeof(struct i2c_reg_tab),
     },
 
     {.addr = 0x2b,
