e8f46c43965fc34c144e2fd9b3beacedb677292a
Kenneth Johansson <kenneth@southpole.se>
2015-02-16 22:37:51 +0100

    perfmngr: indent code.
    use tab = 8 spaces.
    
    no code change.

--- package/peripheral_manager/src/src/led.h
+++ package/peripheral_manager/src/src/led.h
@@ -27,14 +27,14 @@ struct led_drv;
 
 struct led_drv_func{
 	int         (*set_state)(struct led_drv *, led_state_t);	/* Set led state, on,off,flash ...      */
-	led_state_t (*get_state)(struct led_drv *);		/* Get led state, on,off,flash ...	*/
+	led_state_t (*get_state)(struct led_drv *);			/* Get led state, on,off,flash ...	*/
 	int         (*set_color)(struct led_drv *, led_color_t);	/* Set led color			*/
-	led_color_t (*get_color)(struct led_drv *);		/* Get led color			*/
+	led_color_t (*get_color)(struct led_drv *);			/* Get led color			*/
 };
 
 struct led_drv {
 	const char *name;		/* name, set in the confg file,has to be uniq	*/
-	void *priv;		/* for use by the driver			*/
+	void *priv;			/* for use by the driver			*/
 	struct led_drv_func *func;	/* function pointers for controlling the led	*/
 };
 
