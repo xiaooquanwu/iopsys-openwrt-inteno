26db7f11fe4a751c9da7f2415bcfd939fc40a5ee
Kenneth Johansson <kenneth@southpole.se>
2015-02-19 20:13:29 +0100

    LEDS: remake usage of ledctl.
    Remove use of ledctl and use direct ubus instead.
    
    Remove abuse of notice,alert,error states and create new leds instead
    to make it easier read and understand what the different scripts really
    do.
    
    wan_phy_link -> light when link
    wan_phy_speed -> light when speed 1Gb
    
    gbe_phy_lnk -> light wnen link
    gbe_phy_speed -> light when speed 1Gb
    
    new state. eok. to be used when there is more than one led color to indicate
    OK. ex: wifi using green for 2GHz radio and blue for 5GHz radio.

--- package/peripheral_manager/src/src/led.c
+++ package/peripheral_manager/src/src/led.c
@@ -32,13 +32,13 @@ typedef enum {
 static const char * const fn_actions[LED_ACTION_MAX] =
 { "off", "ok", "eok", "notice", "alert", "error", "custom"};
 
-#define LED_FUNCTIONS 17
+#define LED_FUNCTIONS 19
 static const char* const led_functions[LED_FUNCTIONS] =
 { "dsl",	"wifi",		"wps",		"lan",
   "status",	"dect",		"tv",		"usb",
   "wan",	"internet",	"voice1",	"voice2",
   "eco",	"gbe",		"ext",		"wan_phy_speed",
-  "wan_phy_link" };
+  "wan_phy_link","gbe_phy_speed","gbe_phy_link" };
 
 /* Names for led_state_t */
 static const char* const led_states[LED_STATES_MAX] =
@@ -407,6 +407,8 @@ static struct ubus_object led_objects[LED_OBJECTS] = {
     { .name = "led.ext",	.type = &led_object_type, .methods = led_methods, .n_methods = ARRAY_SIZE(led_methods), },
     { .name = "led.wan_phy_speed",	.type = &led_object_type, .methods = led_methods, .n_methods = ARRAY_SIZE(led_methods), },
     { .name = "led.wan_phy_link",	.type = &led_object_type, .methods = led_methods, .n_methods = ARRAY_SIZE(led_methods), },
+    { .name = "led.gbe_phy_speed",	.type = &led_object_type, .methods = led_methods, .n_methods = ARRAY_SIZE(led_methods), },
+    { .name = "led.gbe_phy_link",	.type = &led_object_type, .methods = led_methods, .n_methods = ARRAY_SIZE(led_methods), },
 };
 
 
