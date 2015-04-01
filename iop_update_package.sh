7cea5956da23fc17992000e6f70d5de50ca1e363
Benjamin Larsson <benjamin@southpole.se>
2015-04-01 13:39:10 +0200

    iop_update_package: switch to 3 argument command line, git repo dir, package name, package Makefile

--- iop_update_package.sh
+++ iop_update_package.sh
@@ -2,5 +2,5 @@ cd $1
 NEW_HASH=`git log -n 1 master --pretty=format:"%H"`
 echo $NEW_HASH
 cd ../../..
-sed -r -i  "s/^PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=$NEW_HASH/g" package/$2/Makefile
-git commit package/$2/Makefile -m "$2: update package"
+sed -r -i  "s/^PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=$NEW_HASH/g" $3
+git commit $3 -m "$2: update package"
