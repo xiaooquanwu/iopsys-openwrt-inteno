b8273cc765636815f4c4509063f61d7e231cd006
Martin Schröder <martin.schroder@inteno.se>
2015-04-15 14:18:04 +0200

    Use rpc call directly

--- package/inteno/questd/src/dslstats.c
+++ package/inteno/questd/src/dslstats.c
@@ -34,13 +34,8 @@ void dslstats_init(struct dsl_stats *self){
 	self->traffic = ""; 
 	self->status = ""; 
 	self->link_power_state = ""; 
+	self->line_status = ""; 
 	self->vdsl2_profile = ""; 
-	/*self->snr = (UpDown){0,0}; 
-	self->pwr = (UpDown){0,0}; 
-	self->attn = (UpDown){0, 0}; 
-	self->max_rate = (UpDown){0,0}; 
-	self->msgc = (UpDown){0,0}; 
-	*/
 }
 
 void dslstats_load(struct dsl_stats *self){
@@ -247,80 +242,6 @@ void dslstats_load(struct dsl_stats *self){
 	}
 	
 	pclose(fp);
-	
-	/*
-	local xdsl = sys.exec("xdslctl info --stats")
-	local rv = { }
-
-	rv = {
-		mode	= xdsl:match("Mode:%s+(%S+%s+%S+%s+%S+)") or "",
-		traffic	= xdsl:match("TPS%S+:%s+(%S+)%s+%S+") or "",
-		status	= xdsl:match("Status:%s+(%S+)") or "",
-		lps	= xdsl:match("Link Power State:%s+(%S+)") or "",
-		trldn   = xdsl:match("Trellis:%s+%S+%s+/D:(%S+)%s+") or "",
-		trlup   = xdsl:match("Trellis:%s+U:(%S+)%s+%S+") or "",
-		snrdn   = xdsl:match("SNR%s+%S+%s+(%S+)%s+%S+") or 0,
-		snrup   = xdsl:match("SNR%s+%S+%s+%S+%s+(%S+)") or 0,
-		atndn   = xdsl:match("Attn%S+%s+(%S+)%s+%S+") or 0,
-		atnup   = xdsl:match("Attn%S+%s+%S+%s+(%S+)") or 0,
-		opwdn	= xdsl:match("Pwr%S+%s+(%S+)%s+%S+") or 0,
-		opwup	= xdsl:match("Pwr%S+%s+%S+%s+(%S+)") or 0,
-		artdn	= xdsl:match("Max:%s+%S+%s+%S+%s+%S+%s+%d+%s+%S+%s+Downstream rate = (%d+)%s+%S+") or 0,
-		artup	= xdsl:match("Max:%s+Upstream rate = (%d+)%s+") or 0,
-		rtedn	= xdsl:match("Bearer:%s+%d+%S+%s+%S+%s+%S+%s+%S+%s+%d+%s+%S+%s+Downstream rate = (%d+)%s+%S+") or 0,
-		rteup	= xdsl:match("Bearer:%s+%d+%S+%s+Upstream rate = (%d+)%s+") or 0,
-		msgdn   = xdsl:match("MSGc:%s+(%S+)%s+%S+") or 0,
-		msgup   = xdsl:match("MSGc:%s+%S+%s+(%S+)") or 0,
-		Bdn	= xdsl:match("B:%s+(%S+)%s+%S+") or 0,
-		Bup	= xdsl:match("B:%s+%S+%s+(%S+)") or 0,
-		Mdn	= xdsl:match("M:%s+(%S+)%s+%S+") or 0,
-		Mup	= xdsl:match("M:%s+%S+%s+(%S+)") or 0,
-		Tdn	= xdsl:match("T:%s+(%S+)%s+%S+") or 0,
-		Tup	= xdsl:match("T:%s+%S+%s+(%S+)") or 0,
-		Rdn	= xdsl:match("R:%s+(%S+)%s+%S+") or 0,
-		Rup	= xdsl:match("R:%s+%S+%s+(%S+)") or 0,
-		Sdn	= xdsl:match("S:%s+(%S+)%s+%S+") or 0,
-		Sup	= xdsl:match("S:%s+%S+%s+(%S+)") or 0,
-		Ldn	= xdsl:match("L:%s+(%S+)%s+%S+") or 0,
-		Lup	= xdsl:match("L:%s+%S+%s+(%S+)") or 0,
-		Ddn	= xdsl:match("D:%s+(%S+)%s+%S+") or 0,
-		Dup	= xdsl:match("D:%s+%S+%s+(%S+)") or 0,
-		dlydn	= xdsl:match("delay:%s+(%S+)%s+%S+") or 0,
-		dlyup	= xdsl:match("delay:%s+%S+%s+(%S+)") or 0,
-		inpdn	= xdsl:match("INP:%s+(%S+)%s+%S+") or 0,
-		inpup	= xdsl:match("INP:%s+%S+%s+(%S+)") or 0,
-		frmdn	= xdsl:match("SF:%s+(%S+)%s+%S+") or 0,
-		frmup	= xdsl:match("SF:%s+%S+%s+(%S+)") or 0,
-		sprdn	= xdsl:match("SFErr:%s+(%S+)%s+%S+") or 0,
-		sprup	= xdsl:match("SFErr:%s+%S+%s+(%S+)") or 0,
-		rswdn	= xdsl:match("RS:%s+(%S+)%s+%S+") or 0,
-		rswup	= xdsl:match("RS:%s+%S+%s+(%S+)") or 0,
-		rscdn	= xdsl:match("RSCorr:%s+(%S+)%s+%S+") or 0,
-		rscup	= xdsl:match("RSCorr:%s+%S+%s+(%S+)") or 0,
-		rsudn	= xdsl:match("RSUnCorr:%s+(%S+)%s+%S+") or 0,
-		rsuup	= xdsl:match("RSUnCorr:%s+%S+%s+(%S+)") or 0,
-		hecdn	= xdsl:match("HEC:%s+(%S+)%s+%S+") or 0,
-		hecup	= xdsl:match("HEC:%s+%S+%s+(%S+)") or 0,
-		ocddn	= xdsl:match("OCD:%s+(%S+)%s+%S+") or 0,
-		ocdup	= xdsl:match("OCD:%s+%S+%s+(%S+)") or 0,
-		lcddn	= xdsl:match("LCD:%s+(%S+)%s+%S+") or 0,
-		lcdup	= xdsl:match("LCD:%s+%S+%s+(%S+)") or 0,
-		tcldn	= xdsl:match("Total Cells:%s+(%S+)%s+%S+") or 0,
-		tclup	= xdsl:match("Total Cells:%s+%S+%s+(%S+)") or 0,
-		dcldn	= xdsl:match("Data Cells:%s+(%S+)%s+%S+") or 0,
-		dclup	= xdsl:match("Data Cells:%s+%S+%s+(%S+)") or 0,
-		berdn	= xdsl:match("Bit Errors:%s+(%S+)%s+%S+") or 0,
-		berup	= xdsl:match("Bit Errors:%s+%S+%s+(%S+)") or 0,
-		tesdn	= xdsl:match("ES:%s+(%S+)%s+%S+") or 0,
-		tesup	= xdsl:match("ES:%s+%S+%s+(%S+)") or 0,
-		tssdn	= xdsl:match("SES:%s+(%S+)%s+%S+") or 0,
-		tssup	= xdsl:match("SES:%s+%S+%s+(%S+)") or 0,
-		tuadn	= xdsl:match("UAS:%s+(%S+)%s+%S+") or 0,
-		tuaup	= xdsl:match("UAS:%s+%S+%s+(%S+)") or 0
-	}
-	
-	return rv
-	*/
 }
 
 void dslstats_to_blob_buffer(struct dsl_stats *self, struct blob_buf *b){
