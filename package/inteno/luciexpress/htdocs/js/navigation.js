45a11daf4eac70bb531471e8691701af52c834b0
Simon Kers <simon@inteno.se>
2015-04-21 15:56:23 +0200

    Adding track by $index and removing lambda func.

--- package/inteno/luciexpress/htdocs/js/navigation.js
+++ package/inteno/luciexpress/htdocs/js/navigation.js
@@ -55,7 +55,7 @@ angular.module("luci")
 			Object.assign(o.children, children); 
 			item = o; 
 		}
-		obj.children_list = Object.keys(obj.children).map(key => obj.children[key]);
+		obj.children_list = Object.keys(obj.children).map(function(key) { obj.children[key]; });
 		obj.children_list.sort(function(a, b){
 			return a.index - b.index; 
 		}); 
