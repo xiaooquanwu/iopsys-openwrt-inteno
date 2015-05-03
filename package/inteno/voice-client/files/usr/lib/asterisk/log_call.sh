58dd4c4417e8733fbb5ec41b838dddf0d696d48d
Sukru Senli <sukru.senli@inteno.se>
2015-05-03 11:36:29 +0200

    BB: update voice-client package

--- package/inteno/voice-client/files/usr/lib/asterisk/log_call.sh
+++ package/inteno/voice-client/files/usr/lib/asterisk/log_call.sh
@@ -7,22 +7,35 @@
 # at 100 rows. Some very basic concurrency is supported.
 #
 # Usage:
-# log_call.sh Direction Number [Note]
+# log_call.sh Direction From To [Note]
 #
 # Examples:
-# ./log_call Incoming 07012345678
-# ./log_call Outgoing 07876543210 Blocked
+# ./log_call Incoming 07012345678 07876543210
+# ./log_call Outgoing 07876543210 07012345678 Blocked
 #####################################################
 
-#Check that there is a direction specified
+# Check that there is a direction specified
 if [ -z "$1" ] ; then
 	exit 1
 fi
 
-#Check that there is a number specified
+# Check that there is a calling  number specified
 if [ -z "$2" ] ; then
 	exit 2
 fi
+
+# Check that there is a called number specified
+if [ -z "$3" ] ; then
+	exit 2
+fi
+
+direction=$1
+
+# Remove any suffix starting with an underscore
+from=$(echo $2 | sed 's/_.*//')
+to=$(echo $3 | sed 's/_.*//')
+
+note=$4
 
 logfile="/var/call_log"
 tempfile=$(mktemp)
@@ -30,11 +43,7 @@ now=$(date)
 
 for i in 1 2 3 4 5 ; do
 	if mkdir /var/lock/log_call.lck ; then
-		if ! [ -z $3 ] ; then
-			echo "$now;$1;$2;$3" >> $tempfile
-		else
-			echo "$now;$1;$2" >> 	$tempfile
-		fi
+		echo "$now;$direction;$from;$to;$note" >> $tempfile
 		head -n99 $logfile >> $tempfile
 		mv $tempfile $logfile
 		rmdir /var/lock/log_call.lck
