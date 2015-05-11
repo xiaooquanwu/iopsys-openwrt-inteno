42abc52d0d0e265ca39e05ea06ebd6d418d84c25
Martin Schröder <martin.schroder@inteno.se>
2015-05-11 19:41:16 +0200

    Fixed wifi signal indicator

--- package/inteno/luciexpress/htdocs/plugins/core/widgets/wifi.signal.indicator.js
+++ package/inteno/luciexpress/htdocs/plugins/core/widgets/wifi.signal.indicator.js
@@ -15,10 +15,10 @@ $juci.module("core")
 	$scope.$watch("value", function(value){
 		var q = value / 5; 
 		$scope.bars[0] = $scope.bars[1] = $scope.bars[2] = $scope.bars[3] = false; 
-		if(value > 1) $scope.bars[0] = true; 
-		if(value > 2) $scope.bars[1] = true; 
-		if(value > 3) $scope.bars[2] = true; 
-		if(value > 4) $scope.bars[3] = true; 
+		if(q > 1) $scope.bars[0] = true; 
+		if(q > 2) $scope.bars[1] = true; 
+		if(q > 3) $scope.bars[2] = true; 
+		if(q > 4) $scope.bars[3] = true; 
 	}); 
 	$scope.barStyle = function(idx, active){
 		var height = 5 + ((idx) * 5); 
