41334c7785df03cf85b7afdd0aa7a09ec68c1413
Benjamin Larsson <benjamin@southpole.se>
2015-05-22 16:33:29 +0200

    target: use bcm-base-drivers.sh instead of duplicating our own

--- target/linux/brcm63xx_ref_nand/base-files/lib/brcmref.sh
+++ target/linux/brcm63xx_ref_nand/base-files/lib/brcmref.sh
@@ -23,37 +23,7 @@ bcm_dsl_annex() {
 
 brcm_insmod() {
 	echo Loading brcm modules
-        test -e /lib/modules/3.4.11-rt19/extra/chipinfo.ko     && insmod /lib/modules/3.4.11-rt19/extra/chipinfo.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcmxtmrtdrv.ko  && insmod /lib/modules/3.4.11-rt19/extra/bcmxtmrtdrv.ko
-        test -e /lib/modules/3.4.11-rt19/extra/bcm_ingqos.ko   && insmod /lib/modules/3.4.11-rt19/extra/bcm_ingqos.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcm_bpm.ko      && insmod /lib/modules/3.4.11-rt19/extra/bcm_bpm.ko
-	test -e /lib/modules/3.4.11-rt19/extra/pktflow.ko      && insmod /lib/modules/3.4.11-rt19/extra/pktflow.ko
-	test -e /lib/modules/3.4.11-rt19/extra/pktcmf.ko       && insmod /lib/modules/3.4.11-rt19/extra/pktcmf.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcmfap.ko       && insmod /lib/modules/3.4.11-rt19/extra/bcmfap.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcmxtmcfg.ko    && insmod /lib/modules/3.4.11-rt19/extra/bcmxtmcfg.ko
-	test -e /lib/modules/3.4.11-rt19/extra/adsldd.ko       && insmod /lib/modules/3.4.11-rt19/extra/adsldd.ko
-	test -e /lib/modules/3.4.11-rt19/extra/i2c_bcm6xxx.ko  && insmod /lib/modules/3.4.11-rt19/extra/i2c_bcm6xxx.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcm_enet.ko     && insmod /lib/modules/3.4.11-rt19/extra/bcm_enet.ko
-	test -e /lib/modules/3.4.11-rt19/extra/nciTMSkmod.ko   && insmod /lib/modules/3.4.11-rt19/extra/nciTMSkmod.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcmsw.ko        && insmod /lib/modules/3.4.11-rt19/extra/bcmsw.ko && ifconfig bcmsw up
-#	test -e /lib/modules/3.4.11-rt19/extra/bcm_usb.ko      && insmod /lib/modules/3.4.11-rt19/extra/bcm_usb.ko
-	sleep 1
-	test -e /lib/modules/3.4.11-rt19/extra/bcmarl.ko       && insmod /lib/modules/3.4.11-rt19/extra/bcmarl.ko
-#	/usr/bin/taskset 2>/dev/null
-	test -e /lib/modules/3.4.11-rt19/extra/wfd.ko          && insmod /lib/modules/3.4.11-rt19/extra/wfd.ko
-	test -e /lib/modules/3.4.11-rt19/extra/wlcsm.ko        && insmod /lib/modules/3.4.11-rt19/extra/wlcsm.ko
-	test -e /lib/modules/3.4.11-rt19/extra/wlemf.ko        && insmod /lib/modules/3.4.11-rt19/extra/wlemf.ko
-	test -e /lib/modules/3.4.11-rt19/extra/dhd.ko          && insmod /lib/modules/3.4.11-rt19/extra/dhd.ko  firmware_path=/etc/wlan/dhd mfg_firmware_path=/etc/wlan/dhd/mfg
-	test -e /lib/modules/3.4.11-rt19/extra/wl.ko           && insmod /lib/modules/3.4.11-rt19/extra/wl.ko
-	test -e /lib/modules/3.4.11-rt19/extra/dect.ko         && insmod /lib/modules/3.4.11-rt19/extra/dect.ko
-	test -e /lib/modules/3.4.11-rt19/extra/dectshim.ko     && insmod /lib/modules/3.4.11-rt19/extra/dectshim.ko
-	test -e /lib/modules/3.4.11-rt19/extra/pcmshim.ko      && insmod /lib/modules/3.4.11-rt19/extra/pcmshim.ko
-	test -e /lib/modules/3.4.11-rt19/extra/endpointdd.ko   && insmod /lib/modules/3.4.11-rt19/extra/endpointdd.ko
-	test -e /lib/modules/3.4.11-rt19/extra/p8021ag.ko      && insmod /lib/modules/3.4.11-rt19/extra/p8021ag.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcmvlan.ko      && insmod /lib/modules/3.4.11-rt19/extra/bcmvlan.ko
-	test -e /lib/modules/3.4.11-rt19/extra/pwrmngtd.ko     && insmod /lib/modules/3.4.11-rt19/extra/pwrmngtd.ko
-	test -e /lib/modules/3.4.11-rt19/rng-core.ko           && insmod /lib/modules/3.4.11-rt19/rng-core.ko
-	test -e /lib/modules/3.4.11-rt19/extra/bcmtrng.ko      && insmod /lib/modules/3.4.11-rt19/extra/bcmtrng.ko
+	sh /lib/bcm-base-drivers.sh start
 	echo brcm modules loaded
 }
 
