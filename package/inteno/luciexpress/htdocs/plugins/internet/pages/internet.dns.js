f9655fdfc20437d94d975b860b3771778aead513
Martin Schröder <martin.schroder@inteno.se>
2015-05-25 15:59:29 +0200

    Fixed ddns page

--- package/inteno/luciexpress/htdocs/plugins/internet/pages/internet.dns.js
+++ package/inteno/luciexpress/htdocs/plugins/internet/pages/internet.dns.js
@@ -1,56 +1,32 @@
-$juci.module("internet")
-    .controller("InternetDNSPageCtrl", function ($scope, $uci, $log) {
-
-        $scope.providers = ["dyndns.org"];
-
-        $log.info("uci", $uci);
-
-        $uci.sync(["network","ddns"])
-            .done(function () {
-                if ($uci.network && $uci.network.wan) {
-                    $log.debug("uci wan", $uci.network.wan);
-                    $scope.wan = $uci.network.wan;
-                    if ($scope.wan.dns && $scope.wan.dns.value.length == 0) {
-                        $scope.wan.dns.value = ["",""]; // by default there are 2 DNS addresses
-                    }
-                } else {
-                    $log.error("wan network not available on box");
-                    // TODO show error message
-                }
-                if ($uci.ddns && $uci.ddns.myddns) {
-                    $log.debug("uci myddns", $uci.ddns.myddns);
-                    $scope.ddns = $uci.ddns.myddns;
-                } else {
-                    $log.error("ddns not available on box");
-                    // TODO show error message
-                }
-            }).fail(function () {
-                $log.error("Could not sync network or ddns settings!");
-            })
-            .always(function () {
-                $scope.$apply();
-            });
-
-        $scope.$watch("wan.dns.value", function(value){
-            if (value) {
-                $uci.network.wan.dns.value = value;
-            }
-        }, true);
-
-        $scope.onApply = function () {
-            $scope.busy = 1;
-            $uci.save().done(function () {
-                $log.info("Settings saved!");
-            }).fail(function () {
-                $log.error("Could not save internet settings!");
-            }).always(function () {
-                $scope.$apply();
-                $scope.busy = 0;
-            });
-        };
-
-        $scope.onChangeProvider = function (item) {
-            $uci.ddns.myddns.service_name = item;
-        };
-
-    });
+JUCI.app
+.controller("InternetDNSPageCtrl", function ($scope, $uci, $log) {
+	$scope.providers = ["dyndns.org"];
+	$log.info("uci", $uci);
+	$uci.sync(["network","ddns"]).done(function () {
+		if ($uci.network && $uci.network.wan) {
+			$scope.wan = $uci.network.wan;
+			if ($scope.wan.dns){
+				while($scope.wan.dns.value.length < 2) $scope.wan.dns.value.push("");
+			}
+		} else {
+			// TODO: this should be a dynamic name (wan will not always be called wan in the future)
+			$scope.$emit("Could not find WAN network on this device"); 
+		}
+		if ($uci.ddns && $uci.ddns["@service"]) {
+			$scope.allServices = $uci.ddns["@service"].map(function(x){
+				return { label: x.service_name.value, value: x };
+			});  
+			$scope.ddns = $uci.ddns["@service"][0];
+			$scope.$apply(); 
+		} else {
+			$uci.ddns.create({".type": "service"}).done(function(section){
+				$scope.ddns = section; 
+				$scope.$apply(); 
+			}); 
+		}
+	}); 
+	
+	$scope.onApply = function(){
+		$uci.save(); 
+	} 
+});
