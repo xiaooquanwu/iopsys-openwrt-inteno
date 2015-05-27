884aa51f7b8a35559adf17c86eb1298723e30860
Martin Schröder <martin.schroder@inteno.se>
2015-05-27 11:54:46 +0200

    Separated phone numbers and lines in two files

--- package/inteno/luciexpress/htdocs/plugins/phone/pages/phone.numbers.js
+++ package/inteno/luciexpress/htdocs/plugins/phone/pages/phone.numbers.js
@@ -3,8 +3,8 @@
 JUCI.app
 .controller("PhoneNumbersPageCtrl", function($scope, $uci){
 	$uci.sync("voice_client").done(function(){
-		$scope.phone_lines = $uci.voice_client["@brcm_line"]; 
 		$scope.phone_numbers = $uci.voice_client["@sip_service_provider"]; 
+		$scope.phone_lines = $uci.voice_client["@brcm_line"]; 
 		$scope.allSipAccounts = $scope.phone_numbers.map(function(x){
 			return {
 				label: x.name.value, 
