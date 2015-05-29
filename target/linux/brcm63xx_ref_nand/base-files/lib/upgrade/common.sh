79a30a3e87c367fc6811f2896ed393413c6006aa
Mats Karrman <mats@southpole.se>
2015-05-29 14:04:00 +0200

    base-files upgrade: Reserve some more space for initial UBIFS

--- target/linux/brcm63xx_ref_nand/base-files/lib/upgrade/common.sh
+++ target/linux/brcm63xx_ref_nand/base-files/lib/upgrade/common.sh
@@ -415,7 +415,7 @@ create_springboard_preinit() {
 		echo "Write UBI image..."
 		imagewrite -s 83 -b 0 \
 			   -k $ubifs_ofs -l $ubifs_sz \
-			   --ubi -n 0 -S -6 --vol-name=rootfs_0 \
+			   --ubi -n 0 -S -20 --vol-name=rootfs_0 \
 			   /dev/mtd$mtd_no $from
 		echo "Write kernel..."
 		imagewrite -c -s 3 -b 40 -k $k_ofs -l $k_sz \
