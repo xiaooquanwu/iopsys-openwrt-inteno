24656406d7d0211edc975527820a31c88b6a7b5d
Mats Karrman <mats@southpole.se>
2015-06-02 11:26:23 +0200

    base-files upgrade: Reserve some more space for initial UBIFS (2)

--- target/linux/brcm63xx_ref_nand/base-files/lib/upgrade/common.sh
+++ target/linux/brcm63xx_ref_nand/base-files/lib/upgrade/common.sh
@@ -553,7 +553,7 @@ inteno_image_upgrade() {
 				v "Write UBI image..."
 				imagewrite -s 83 -b 0 \
 					   -k $ubifs_ofs -l $ubifs_sz \
-					   --ubi -n 0 -S -6 --vol-name=rootfs_0 \
+					   --ubi -n 0 -S -20 --vol-name=rootfs_0 \
 					   /dev/mtd$mtd_no $from
 
 				v "Write kernel..."
