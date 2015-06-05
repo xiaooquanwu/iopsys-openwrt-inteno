f64e010c383d2c597bf72cfd7cc64f5009e11a89
Martin Schröder <martin.schroder@inteno.se>
2015-06-05 10:25:30 +0200

    Removed luciexpress http parts from luciexpress (now they are in juci package)

--- package/inteno/luciexpress/htdocs/plugins/settings/settings.js
+++
@@ -1,8 +0,0 @@
-angular.module("luci").config(function($stateProvider) {
-	$stateProvider.state("settings", {
-		url: "/settings", 
-		onEnter: function($state){
-			$juci.redirect("settings-password"); 
-		},
-	});
-}); 
