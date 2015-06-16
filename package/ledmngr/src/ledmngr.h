4286cb5213d7192cd8dca24b167eb30eff055cee
Martin Schröder <mkschreder.uk@gmail.com>
2015-06-16 17:45:23 +0200

    Changes to the config and netif patch

--- package/ledmngr/src/ledmngr.h
+++
@@ -1,3 +0,0 @@
-
-int board_ioctl(int ioctl_id, int action, int hex, char* string_buf, int string_buf_len, int offset);
-extern int brcmboard;
--- package/ledmngr/src/spi.h
+++
@@ -1,2 +0,0 @@
-int spi_init(struct uci_context *uci_ctx);
-void spi_led_set(struct led_config* lc, int state);
