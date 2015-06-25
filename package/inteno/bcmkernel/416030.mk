unofficial-inteno-public-mirror/iopsys-barrier-breaker

Public SDK: Build using a special option to normal makefile, refs #5781
Before the Public SDK was built using a separate package.
Now that it is possible to choose between several different kernel
versions, this does not work. Instead a new menuconfig option to the
normal bcmkernel package has been added that makes it build the
Public SDK of the current release.

NOTE: Make sure that the kernel version selected in menuconfig
      matches the saved Public SDK tarball of the current release.

Only the latest release (4.16L.03) has been tested, the older version
builds are killed with an eror message if Public SDK is selected.

package/bcmkernel/416030.mk
@@ -8,6 +8,7 @@
 # update this based on the Broadcom SDK version, 4.16L.03  -> 416030
 BRCM_SDK_VERSION:=416030
 
+ifneq ($(CONFIG_BCM_OPEN),y)
 PKG_NAME:=bcmkernel-3.4
 PKG_VERSION:=4.16
 PKG_RELEASE:=$(BRCM_SDK_VERSION)
@@ -15,8 +16,9 @@ PKG_RELEASE:=$(BRCM_SDK_VERSION)
 PKG_SOURCE_URL:=git@iopsys.inteno.se:bcmkernel-4.16L.03
 PKG_SOURCE_PROTO:=git
 
-PKG_SOURCE_VERSION:=1e02f70dfa3f80e79965129c957b40219d5eda0b
+PKG_SOURCE_VERSION:=b3305f9a6119546745f5854cb69782738715d8aa
 PKG_SOURCE:=$(PKG_NAME)-$(BRCM_SDK_VERSION)-$(PKG_SOURCE_VERSION).tar.gz
+endif
 
 PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)
 
@@ -154,7 +156,9 @@ define Package/bcmkernel/install
 
 	$(CP) $(PKG_BUILD_DIR)/$(BCM_SDK_VERSION)/bcmdrivers/broadcom/include/bcm963xx/endptvoicestats.h $(STAGING_DIR)/usr/include/bcm963xx/bcmdrivers/broadcom/include/bcm963xx/endptvoicestats.h
 	$(CP) $(PKG_BUILD_DIR)/$(BCM_SDK_VERSION)/bcmdrivers/broadcom/include/bcm963xx/endpointdrv.h $(STAGING_DIR)/usr/include/bcm963xx/bcmdrivers/broadcom/include/bcm963xx/endpointdrv.h
+    ifneq ($(CONFIG_BCM_OPEN),y)
 	$(CP) $(PKG_BUILD_DIR)/$(BCM_SDK_VERSION)/userspace/private/apps/vodsl/voip/inc/tpProfiles.h $(STAGING_DIR)/usr/include/bcm963xx/userspace/private/apps/vodsl/voip/inc
+    endif
 	echo "#define BCM_SDK_VERSION $(BRCM_SDK_VERSION)" > $(STAGING_DIR)/usr/include/bcm_sdk_version.h
 
 	# create symlink to kernel build directory

xiaooquanwu/iopsys-openwrt-inteno

package/inteno/bcmkernel/416030.mk
@@ -8,6 +8,7 @@
 # update this based on the Broadcom SDK version, 4.16L.03  -> 416030
 BRCM_SDK_VERSION:=416030
 
+ifneq ($(CONFIG_BCM_OPEN),y)
 PKG_NAME:=bcmkernel-3.4
 PKG_VERSION:=4.16
 PKG_RELEASE:=$(BRCM_SDK_VERSION)
@@ -15,8 +16,9 @@ PKG_RELEASE:=$(BRCM_SDK_VERSION)
 PKG_SOURCE_URL:=git@iopsys.inteno.se:bcmkernel-4.16L.03
 PKG_SOURCE_PROTO:=git
 
-PKG_SOURCE_VERSION:=e176e44f704382aeb36019afe96d323216b0f19c
+PKG_SOURCE_VERSION:=c83b5c491f630e95eb902e6a4bdad00e51fcb0e7
 PKG_SOURCE:=$(PKG_NAME)-$(BRCM_SDK_VERSION)-$(PKG_SOURCE_VERSION).tar.gz
+endif
 
 PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)
 
@@ -155,7 +157,9 @@ define Package/bcmkernel/install
 
 	$(CP) $(PKG_BUILD_DIR)/$(BCM_SDK_VERSION)/bcmdrivers/broadcom/include/bcm963xx/endptvoicestats.h $(STAGING_DIR)/usr/include/bcm963xx/bcmdrivers/broadcom/include/bcm963xx/endptvoicestats.h
 	$(CP) $(PKG_BUILD_DIR)/$(BCM_SDK_VERSION)/bcmdrivers/broadcom/include/bcm963xx/endpointdrv.h $(STAGING_DIR)/usr/include/bcm963xx/bcmdrivers/broadcom/include/bcm963xx/endpointdrv.h
+    ifneq ($(CONFIG_BCM_OPEN),y)
 	$(CP) $(PKG_BUILD_DIR)/$(BCM_SDK_VERSION)/userspace/private/apps/vodsl/voip/inc/tpProfiles.h $(STAGING_DIR)/usr/include/bcm963xx/userspace/private/apps/vodsl/voip/inc
+    endif
 	echo "#define BCM_SDK_VERSION $(BRCM_SDK_VERSION)" > $(STAGING_DIR)/usr/include/bcm_sdk_version.h
 
 	# create symlink to kernel build directory